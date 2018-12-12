4e0351c70b68c8175a3ac5c970c936382bd637ed
feresr <fernandoraviola@gmail.com>
2018-12-12 18:26:43 -0300

    .

--- computer/src/main/Keyboard.kt
+++ computer/src/main/Keyboard.kt
@@ -2,14 +2,23 @@ import java.awt.event.KeyEvent
 import java.awt.event.KeyListener
 
 class Keyboard(private val memory: Register) : KeyListener {
-    override fun keyTyped(e: KeyEvent?) {}
 
-    override fun keyPressed(e: KeyEvent) {
-        println("scan code ${e.paramString().split(",")}")
-        memory(e.paramString().split(",")[6].split("=")[1].toShort(), true)
+    override fun keyTyped(e: KeyEvent) {
     }
 
+    override fun keyPressed(e: KeyEvent) {
+        memory(e.keyCode.toKey(), true)
+    }
+
+    //TODO: MAP CORRECT SCANKEY
+    private fun Int.toKey() = when(this) {
+        37 -> 130
+        39 -> 132
+        27 -> 140
+        else -> this
+    }.toShort()
+
     override fun keyReleased(e: KeyEvent?) {
-        memory(0, true)
+        memory(0.toShort(), true)
     }
 }
